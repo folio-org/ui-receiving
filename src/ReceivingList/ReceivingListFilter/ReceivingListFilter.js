74d0180fda9c430dc9a8d35b279d82b9e1b2d228
Andrei Shumski2 <Andrei_Shumski2@epam.com>
2020-05-11 14:29:35 +0300

    add uniq ids for filters

--- src/ReceivingList/ReceivingListFilter/ReceivingListFilter.js
+++ src/ReceivingList/ReceivingListFilter/ReceivingListFilter.js
@@ -34,7 +34,7 @@ const ReceivingListFilter = ({
   return (
     <AccordionSet>
       <AcqCheckboxFilter
-        id={FILTERS.ORDER_STATUS}
+        id={`filter-${FILTERS.ORDER_STATUS}`}
         activeFilters={activeFilters[FILTERS.ORDER_STATUS]}
         labelId="ui-receiving.filter.orderStatus"
         name={FILTERS.ORDER_STATUS}
@@ -43,7 +43,7 @@ const ReceivingListFilter = ({
       />
 
       <PluggableOrganizationFilter
-        id={FILTERS.ORDER_ORGANIZATION}
+        id={`filter-${FILTERS.ORDER_ORGANIZATION}`}
         activeFilters={activeFilters[FILTERS.ORDER_ORGANIZATION]}
         labelId="ui-receiving.filter.vendor"
         name={FILTERS.ORDER_ORGANIZATION}
@@ -51,7 +51,7 @@ const ReceivingListFilter = ({
       />
 
       <AcqCheckboxFilter
-        id={FILTERS.ORDER_TYPE}
+        id={`filter-${FILTERS.ORDER_TYPE}`}
         activeFilters={activeFilters[FILTERS.ORDER_TYPE]}
         labelId="ui-receiving.filter.orderType"
         name={FILTERS.ORDER_TYPE}
@@ -60,7 +60,7 @@ const ReceivingListFilter = ({
       />
 
       <MaterialTypeFilterContainer
-        id={FILTERS.MATERIAL_TYPE}
+        id={`filter-${FILTERS.MATERIAL_TYPE}`}
         activeFilters={activeFilters[FILTERS.MATERIAL_TYPE]}
         labelId="ui-receiving.filter.materialType"
         name={FILTERS.MATERIAL_TYPE}
@@ -68,7 +68,7 @@ const ReceivingListFilter = ({
       />
 
       <AcqCheckboxFilter
-        id={FILTERS.ORDER_FORMAT}
+        id={`filter-${FILTERS.ORDER_FORMAT}`}
         activeFilters={activeFilters[FILTERS.ORDER_FORMAT]}
         labelId="ui-receiving.filter.orderFormat"
         name={FILTERS.ORDER_FORMAT}
@@ -77,14 +77,14 @@ const ReceivingListFilter = ({
       />
 
       <AcqTagsFilter
-        id={FILTERS.POL_TAGS}
+        id={`filter-${FILTERS.POL_TAGS}`}
         activeFilters={activeFilters[FILTERS.POL_TAGS]}
         name={FILTERS.POL_TAGS}
         onChange={adaptedApplyFilters}
       />
 
       <LocationFilterContainer
-        id={FILTERS.LOCATION}
+        id={`filter-${FILTERS.LOCATION}`}
         activeFilter={activeFilters[FILTERS.LOCATION] && activeFilters[FILTERS.LOCATION][0]}
         labelId="ui-receiving.filter.location"
         name={FILTERS.LOCATION}
@@ -92,6 +92,7 @@ const ReceivingListFilter = ({
       />
 
       <AcqCheckboxFilter
+        id={`filter-${FILTERS.RECEIVING_STATUS}`}
         activeFilters={activeFilters[FILTERS.RECEIVING_STATUS]}
         labelId="ui-receiving.filter.receivingStatus"
         name={FILTERS.RECEIVING_STATUS}
@@ -100,7 +101,7 @@ const ReceivingListFilter = ({
       />
 
       <AcqUnitFilter
-        id={FILTERS.ACQUISITIONS_UNIT}
+        id={`filter-${FILTERS.ACQUISITIONS_UNIT}`}
         activeFilters={activeFilters[FILTERS.ACQUISITIONS_UNIT]}
         labelId="ui-receiving.filter.acqUnits"
         name={FILTERS.ACQUISITIONS_UNIT}
