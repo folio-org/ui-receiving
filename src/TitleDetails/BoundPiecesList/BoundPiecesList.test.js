8a907099ae79e97e2839c4782cf302a0fce00e64
Alisher Musurmonov <alisher_musurmonov1@epam.com>
2024-06-12 18:43:15 +0500

    tests: update test mocks

--- src/TitleDetails/BoundPiecesList/BoundPiecesList.test.js
+++ src/TitleDetails/BoundPiecesList/BoundPiecesList.test.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import { render, cleanup } from '@folio/jest-config-stripes/testing-library/react';
+import {
+  render,
+  cleanup,
+  screen,
+} from '@folio/jest-config-stripes/testing-library/react';
 import { IntlProvider } from 'react-intl';
 
 import '@folio/stripes-acq-components/test/jest/__mock__';
@@ -7,6 +10,10 @@ import '@folio/stripes-acq-components/test/jest/__mock__';
 import { usePaginatedPieces } from '../../common/hooks';
 import { BoundPiecesList } from './BoundPiecesList';
 
+jest.mock('@folio/stripes/components', () => ({
+  ...jest.requireActual('@folio/stripes/components'),
+  TextLink: jest.fn().mockImplementation(({ children, to }) => <a href={to} data-testid="textLink">{children}</a>),
+}));
 jest.mock('@folio/stripes/core', () => ({
   ...jest.requireActual('@folio/stripes/core'),
   useStripes: jest.fn().mockReturnValue({ hasPerm: jest.fn().mockReturnValue(true) }),
@@ -22,12 +29,13 @@ const pieces = [{
   status: { name: 'Available' },
 }];
 
-const renderBoundPiecesList = () => (render(
+const renderBoundPiecesList = (props = {}) => (render(
   <IntlProvider locale="en">
     <BoundPiecesList
       id="boundPiecesListId"
       filters={{}}
       title={{ id: 'titleId' }}
+      {...props}
     />
   </IntlProvider>,
 ));
@@ -44,13 +52,44 @@ describe('BoundPiecesList', () => {
   afterEach(cleanup);
 
   it('should render component', () => {
-    const { getByText } = renderBoundPiecesList();
+    renderBoundPiecesList();
 
-    expect(getByText('ui-receiving.piece.callNumber')).toBeInTheDocument();
-    expect(getByText('ui-receiving.piece.displaySummary')).toBeInTheDocument();
-    expect(getByText('ui-receiving.piece.barcode')).toBeInTheDocument();
-    expect(getByText('ui-receiving.piece.status')).toBeInTheDocument();
+    expect(screen.getByText('ui-receiving.piece.callNumber')).toBeInTheDocument();
+    expect(screen.getByText('ui-receiving.piece.displaySummary')).toBeInTheDocument();
+    expect(screen.getByText('ui-receiving.piece.barcode')).toBeInTheDocument();
+    expect(screen.getByText('ui-receiving.piece.status')).toBeInTheDocument();
 
-    expect(getByText(pieces[0].barcode)).toBeInTheDocument();
+    expect(screen.getByText(pieces[0].barcode)).toBeInTheDocument();
+  });
+
+  it('should render barcode link', () => {
+    usePaginatedPieces.mockClear().mockReturnValue({
+      pieces: [{
+        ...pieces[0],
+        itemId: 'itemId',
+        holdingsRecordId: 'holdingsRecordId',
+      }],
+      totalCount: pieces.length,
+      isFetching: false,
+    });
+
+    renderBoundPiecesList({ title: { instanceId: 'instanceId' } });
+
+    expect(screen.getByText('ui-receiving.piece.barcode')).toBeInTheDocument();
+    expect(screen.getByTestId('textLink')).toBeInTheDocument();
+
+    expect(screen.getByText(pieces[0].barcode)).toBeInTheDocument();
+  });
+
+  it('should not render component when pieces are not fetched', () => {
+    usePaginatedPieces.mockReturnValue({
+      pieces: null,
+      totalCount: 0,
+      isFetching: false,
+    });
+
+    renderBoundPiecesList();
+
+    expect(screen.queryByText('ui-receiving.piece.displaySummary')).not.toBeInTheDocument();
   });
 });
