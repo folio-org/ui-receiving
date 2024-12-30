f5a00761b8d2220890e7248e1cf9f3ea466982e9
Yury Saukou <yury_saukou@epam.com>
2024-12-30 12:59:55 +0400

    UIREC-427 Chunk HTTP requests to get pieces 'requests' by piece IDs (#625)

--- src/common/hooks/usePiecesRequests/usePiecesRequests.js
+++ src/common/hooks/usePiecesRequests/usePiecesRequests.js
@@ -1,3 +1,4 @@
+import chunk from 'lodash/chunk';
 import { useQuery } from 'react-query';
 
 import {
@@ -5,7 +6,11 @@ import {
   useOkapiKy,
 } from '@folio/stripes/core';
 
-import { PIECE_REQUESTS_API } from '../../constants';
+import {
+  CHUNK_SIZE,
+  MAX_PARALLEL_REQUESTS,
+  PIECE_REQUESTS_API,
+} from '../../constants';
 import { buildPieceRequestsSearchParams } from '../../utils';
 
 const DEFAULT_DATA = [];
@@ -26,10 +31,28 @@ export const usePiecesRequests = (pieces = [], options = {}) => {
     isLoading,
   } = useQuery({
     queryKey: [namespace, tenantId, pieces],
-    queryFn: ({ signal }) => {
-      const searchParams = buildPieceRequestsSearchParams(pieces);
+    queryFn: async ({ signal }) => {
+      const chunks = chunk(pieces, CHUNK_SIZE);
+      const chunkGroups = chunk(chunks, MAX_PARALLEL_REQUESTS);
 
-      return ky.get(PIECE_REQUESTS_API, { searchParams, signal }).json();
+      const initialAccumulator = { circulationRequests: [], totalRecords: 0 };
+
+      for (const group of chunkGroups) {
+        const responses = await Promise.all(
+          group.map(async (_chunk) => {
+            const searchParams = buildPieceRequestsSearchParams(_chunk);
+
+            return ky.get(PIECE_REQUESTS_API, { searchParams, signal }).json();
+          }),
+        );
+
+        responses.forEach((response) => {
+          initialAccumulator.circulationRequests.push(...response.circulationRequests);
+          initialAccumulator.totalRecords += response.totalRecords;
+        });
+      }
+
+      return initialAccumulator;
     },
     enabled: enabled && Boolean(pieces.length),
     ...queryOptions,
