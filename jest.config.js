27b1c9635302fcc0e007d7322c06eaf4d8ef65cd
Yury Saukou <yury_saukou@epam.com>
2026-09-11 18:45:59 +0400

    Update stripes deps

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
-const path = require('path');
-const stripesConfig = require('@folio/jest-config-stripes');
+const path = require('node:path');
+const { config: stripesConfig } = require('@folio/jest-config-stripes');
 const acqConfig = require('@folio/stripes-acq-components/jest.config');
+
+const transformIgnorePatterns = stripesConfig.transformIgnorePatterns.map((pattern) => pattern.replace('(?!', '(?!@k-int/stripes-kint-components|'));
 
 module.exports = {
   ...stripesConfig,
@@ -9,7 +11,5 @@ module.exports = {
     ...acqConfig.setupFiles,
     path.join(__dirname, './test/jest/setupFiles.js'),
   ],
-  transformIgnorePatterns: [
-    '/node_modules/(?!@k-int/stripes-kint-components|@folio/.*|ky/.*|uuid/.*)',
-  ],
+  transformIgnorePatterns,
 };
